af3f7f67fe299d68eceaa91ce0a1149de7e28bce
Aakash-900 <aakashchdhry96@gmail.com>
2024-08-13 18:20:28 +0545

    deleted the unwanted code

--- models/User.js
+++ models/User.js
@@ -1,23 +1,3 @@
-// User.js
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//   firstName: { type: String, required: true },
-//   lastName: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true, select: false },
-//   phone: { type: String },
-//   address: { type: String },
-//   nation: { type: String },
-//   gender: { type: String },
-//   dob: { type: Date },
-//   profileImage: { type: String },
-//   role: { type: String, enum: ['user', 'admin'], default: 'user' },
-//   resetPasswordToken: { type: String },
-//   resetPasswordExpires: { type: Date }
-// });
-
-// module.exports = mongoose.model('User', userSchema);
 
 const mongoose = require('mongoose');
 
