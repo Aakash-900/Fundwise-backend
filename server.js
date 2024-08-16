5af45bbc722739d7fc45e689b997a763ef63d388
Aakash-900 <aakashchdhry96@gmail.com>
2024-08-16 20:39:27 +0545

    update

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.delete('/api/admin/users/:id', authMiddleware, deleteUser);
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-
+// Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
